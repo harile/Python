d574b8b0557810e1d82046d2750aa1ce0f13cec5
harile <lvhai17@gmail.com>
2021-08-20 17:15:45 +0700

    Create CoronaBot Update status simple

---
+++ CovidCaseStatusBot.py
@@ -0,0 +1,17 @@
+import requests
+from win10toast import ToastNotifier
+import json
+import time
+
+def update():
+     r = requests.get('https://coronavirus-19-api.herokuapp.com/all')
+     data = r.json()
+     text =  f'Coronavirus cases:\t{data["cases"]}\nDeaths:\t{data["deaths"]}\nRecovered:\t{data["recovered"]}'
+
+     while True:
+         t =  ToastNotifier()
+         t.show_toast("Covid19 Update", text, duration = 20)
+         time.sleep(30)
+
+update()
+
